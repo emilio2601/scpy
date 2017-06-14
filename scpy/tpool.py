eca8373c0118c814c750bf9b2be23cb6e7d7a31d
Emilio Mendoza <emilio2601@gmail.com>
2017-06-14 10:44:06 -0500

    Added tpool module

--- scpy/tpool.py
+++ scpy/tpool.py
@@ -1,3 +1,12 @@
 class SiaTPool(object):
     def __init__(self, scpy):
         self.scpy = scpy
+
+    def fee(self):
+        return self.scpy.get_api('/tpool/fee')
+
+    def get_raw(self, id):
+        return self.scpy.get_api(f'/tpool/raw/{id}')
+
+    def submit_raw(self, parents, transaction):
+        return self.scpy.post_api('/tpool/raw', data={'parents': parents, 'transaction': transaction})
